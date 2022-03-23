7818b51056caf0e77c015a2bbcb09fe27437143e
Kayser <86619328+KayserWebDeveloper@users.noreply.github.com>
2022-03-23 13:11:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const { stringify } = require("querystring");
 
 const app = express();
 app.use(bodyParser.json());
